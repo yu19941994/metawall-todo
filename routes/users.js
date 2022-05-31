c9cc8e087d2cd83445f52510a829899d93742fd4
yu19941994 <yishian.yu@gmail.com>
2022-05-31 23:49:42 +0800

    feat: adjust routes/user

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    // res.send('respond with a resource');
+    res.status(200).json({
+        "name": "使用者"
+    })
+});
+
+router.get('/login', function(req, res, next) {
+    // res.send('respond with a resource');
+    res.status(200).json({
+        "name": "使用者"
+    })
 });
 
 module.exports = router;

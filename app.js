dc80a809dfded9b98a1cdec8bc730974ba67fa8a
yu19941994 <yishian.yu@gmail.com>
2022-06-03 20:01:22 +0800

    feat: add uncaughtException

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
+// 捕捉程式紀錄
+process.on('uncaughtException', err => {
+  // 記錄錯誤下來，等到服務都處理完後，停掉該 process
+	console.error('Uncaughted Exception！')
+	console.error(err);
+	process.exit(1);
+});
+
 // router
 const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');

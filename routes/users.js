e17ec81a74ee10f09ebe195934673bf96771b9b9
yu19941994 <yishian.yu@gmail.com>
2022-06-05 22:29:38 +0800

    feat: add user model and handleErrorAsync to routes/users

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../model/user');
+const handleErrorAsync = require('../service/handleErrorAsync');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');

ab393221d02880bd8fed6a444a69266f302c56e2
yu19941994 <yishian.yu@gmail.com>
2022-06-05 20:41:54 +0800

    fix: remove useless code

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,4 @@ const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    // res.send('respond with a resource');
-    res.status(200).json({
-        "name": "使用者"
-    })
-});
-
-router.get('/login', function(req, res, next) {
-    // res.send('respond with a resource');
-    res.status(200).json({
-        "name": "使用者"
-    })
-});
-
 module.exports = router;

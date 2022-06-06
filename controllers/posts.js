1c186f0f6697b6ef338ba2b8a3597b7b1474f69c
yu19941994 <yishian.yu@gmail.com>
2022-06-07 01:28:47 +0800

    Revert "feat: 將測試 id 改為使用者 id"
    This reverts commit 956d3fa8feac9a6fb57bc64e4f928ce9ae9b742e.

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,13 +16,15 @@ const posts = {
     },
     async createPost(req, res, next) {
         const { body } = req;
+        // 固定 ID
+        const userId = '629635e3a29b078d01109de9';
         if (!!body.content) {
             const newPost = await Post.create(
                 {
                     content: body.content,
                     image: body.image,
                     name: body.name,
-                    user: req.user.id,
+                    user: userId,
                     likes: body.likes
                 }
             )
